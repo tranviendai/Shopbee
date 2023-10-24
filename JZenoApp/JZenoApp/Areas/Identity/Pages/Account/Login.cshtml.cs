9497d51a259ce1bfb7915584fdeeaae9f4ab1932
nguyenle <nguyentaro1502@gmail.com>
2023-10-24 19:20:41 +0700

    the first commit

--- JZenoApp/JZenoApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ JZenoApp/JZenoApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace JZenoApp.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Ghi nhớ")]
             public bool RememberMe { get; set; }
         }
 
